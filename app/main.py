f5046b918ba5314096b86a481d3e025ab4908ab4
nyi137 <55394672+nyi137@users.noreply.github.com>
2019-09-16 12:05:29 -0400

    Update main.py

--- app/main.py
+++ app/main.py
@@ -41,9 +41,7 @@ def start():
     """
     print(json.dumps(data))
 
-    color: "#ff00ff",
-    headType: "bendr",
-    tailType: "pixel"
+    color: "#ff00ff"
 
     return start_response(color)
 
